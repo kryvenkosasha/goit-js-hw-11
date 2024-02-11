6ef370b2eea5b42b87be247c467a5d450cc09bed
kryvenkosasha <143883449+kryvenkosasha@users.noreply.github.com>
2024-02-11 12:10:47 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
-
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const refs = {
   searchForm: document.querySelector('form'),
@@ -14,10 +13,26 @@ refs.searchForm.addEventListener('submit', e => {
   e.preventDefault();
   if (refs.searchInput.value === '') return;
 
-  const value = e.target.elements.query.value;
+  const value = refs.searchInput.value;
   searchImages(value);
 });
 
-function searchImages(userValue) {}
+function searchImages(userValue) {
+  const BASE_URL = 'https://pixabay.com/api/';
+  const END_POINT = '/api/';
+  const url = BASE_URL + END_POINT;
+
+  const options = {
+    headers: {
+      key: '42312276-5bc23f4af127c6565aecd0d27',
+      q: `${userValue}`,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: 'true',
+    },
+  };
+
+  return fetch(url, options).then(res => console.log(res));
+}
 
 function createMarcup() {}
